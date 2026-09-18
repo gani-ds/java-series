66f2ada491ea00c1b19ce28d6f563171307a619e
gani-ds <myspace171717@gmail.com>
2026-09-18 22:51:41 +0530

    Add addition of two elements.java

---
+++ addition of two elements.java
@@ -0,0 +1,10 @@
+class AddNumbers{
+    public static void main(string[]args){
+        int a =10;
+        int b =20;
+
+        int sum = a+b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
