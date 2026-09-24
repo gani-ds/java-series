5b2876910d4b8411d391599e3325a41795405b7e
gani-ds <myspace171717@gmail.com>
2026-09-24 23:11:03 +0530

    Add greatestnumber.java

---
+++ greatestnumber.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 40;
+
+        if (a > b) {
+            System.out.println(a + " is larger");
+        } else {
+            System.out.println(b + " is larger");
+        }
+    }
+}
